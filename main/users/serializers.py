72776552a7ee5743b8bbdac52945b41c245f65fe
Kashinath11 <bken25gourkhali@gmail.com>
2023-11-01 21:51:58 +0545

    setting_up_user_registration

--- main/users/serializers.py
+++ main/users/serializers.py
@@ -6,6 +6,14 @@ class UserSerializer(serializers.ModelSerializer):
     
     password = serializers.CharField(write_only=True, required=False)
     
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        
+        return user
+    
     class Meta:
         model = User
         fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'email', 'password']
