fafce1976f784cb52407f9a56c81a9a55aaa3122
Kashinath11 <bken25gourkhali@gmail.com>
2023-11-01 21:38:00 +0545

    password_field_added_in_serailizer

--- main/main/urls.py
+++ main/main/urls.py
@@ -25,5 +25,5 @@ api_url_patterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_url_patterns))
-    # path('api-auth/', include('rest_framework.urls'))
+    
 ]
--- main/users/serializers.py
+++ main/users/serializers.py
@@ -4,7 +4,9 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     
+    password = serializers.CharField(write_only=True, required=False)
+    
     class Meta:
         model = User
-        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'email']
+        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'email', 'password']
         
