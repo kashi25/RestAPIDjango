60b06820253e60f9bfc1bda58ad0c7214d77925e
Kashinath11 <bken25gourkhali@gmail.com>
2023-11-15 22:11:51 +0545

    password_update_feature_added

--- main/users/serializers.py
+++ main/users/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     
     password = serializers.CharField(write_only=True, required=False)
+    old_password = serializers.CharField(write_only=True, required=False)
     username = serializers.CharField(read_only=True)
     
     def create(self, validated_data):
@@ -15,7 +16,22 @@ class UserSerializer(serializers.ModelSerializer):
         
         return user
     
+    def update(self, instance, validated_data):
+        try:
+            user = instance
+            password = validated_data.pop('password')
+            old_password = validated_data.pop('old_password')
+            if user.check_password(old_password):
+                user.set_password(password)
+            else:
+                raise Exception("old password is incorrect")
+            user.save()
+        except Exception as err:
+            raise serializers.ValidationError(err)
+        return super(UserSerializer, self).update(instance, validated_data)
+            
+    
     class Meta:
         model = User
-        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'email', 'password']
+        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'email', 'password', 'old_password']
         
