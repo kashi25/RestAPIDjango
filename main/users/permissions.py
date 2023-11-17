a2f5706e925bc35941bd5a6397f5ee999236a021
Kashinath11 <bken25gourkhali@gmail.com>
2023-11-17 13:29:56 +0545

    new_updated

--- main/users/permissions.py
+++ main/users/permissions.py
@@ -1,15 +1,16 @@
 from rest_framework import permissions
 
-class IsUserOwnerOrGetAndPostOnly():
+class IsUserOwnerOrGetAndPostOnly(permissions.BasePermission):
     """
-    custom permission for UserViewSet to only aloow user to edit their own profile. Otherwise, Get and Post Only 
+    Custom permission for UserViewSet to only allow users to edit their own profile. Otherwise, Get and Post Only.
     """
     
     def has_permission(self, request, view):
-        return True
+        if request.method in permissions.SAFE_METHODS or request.method == 'POST':
+            return True
+        return False
     
     def has_object_permission(self, request, view, obj):
-        
         if request.method in permissions.SAFE_METHODS:
             return True
         
--- main/users/signals.py
+++ main/users/signals.py
@@ -4,18 +4,19 @@ from django.dispatch import receiver
 
 from .models import Profile
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
 @receiver(pre_save, sender=User)
 def set_username(sender, instance, **kwargs):
     if not instance.username:
         username = f'{instance.first_name}_{instance.last_name}'.lower()
         counter = 1
-        while User.objects.filter(username=username):
+        max_attempts = 100  # Set a maximum number of attempts to avoid infinite loop
+
+        while User.objects.filter(username=username).exists() and counter <= max_attempts:
             username = f'{instance.first_name}_{instance.last_name}_{counter}'.lower()
             counter += 1
+
+        # If the loop reaches the maximum attempts, you may want to handle it appropriately.
+        if counter > max_attempts:
+            raise ValueError("Unable to generate a unique username.")
+
         instance.username = username
